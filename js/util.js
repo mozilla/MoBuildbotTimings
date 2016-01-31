833a9012039aba51269c6616a87bd80f590e05cd
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-01-30 20:40:20 -0500

    looking better

--- js/util.js
+++ js/util.js
@@ -11,11 +11,10 @@ importScript("../modevlib/MozillaPrograms.js");
 importScript("../modevlib/qb/ESQuery.js");
 importScript("../modevlib/charts/cccChart.js");
 importScript("../modevlib/charts/aColor.js");
-importScript([
-	"../css/menu.css"
-]);
+importScript("../css/menu.css");
 importScript("../modevlib/math/Stats.js");
 importScript("../modevlib/qb/Qb.js");
+importScript("../modevlib/charts/tools.js");
 
 
 var search = function*(query){
@@ -62,4 +61,169 @@ function sidebarSlider(){
 		}//endif
 	});
 }
+
+
+function showTimeline(action, timings){
+	var width = 800;
+	var colors = aChart.STYLES.select("color");
+	var BORDER = 2;
+	var HEIGHT = 30;
+	var LINE_PADDING = 10;
+
+	var xScale = d3.scale.linear().domain([action.start_time, action.end_time]).range([20, width - 50]);
+
+	function start(d){
+		return xScale(coalesce(Map.get(d, "harness.start_time"), Map.get(d, "builder.start_time")));
+	}
+
+	function end(d){
+		return xScale(coalesce(Map.get(d, "harness.end_time"), Map.get(d, "builder.end_time")));
+	}
+
+	function duration(d){
+		return end(d) - start(d);
+	}
+
+	function level(d){
+		return (Map.get(d, "harness.start_time") ? 1 : 0)
+	}
+
+	function yLine(d){
+		return LINE_PADDING + (HEIGHT + LINE_PADDING) * level(d);
+	}
+
+
+	var svg = d3.select("#timeline")
+			.html('<h3 style="padding-top:20px;">Timeline</h3>')
+			.append("svg")
+			.width(width)
+			.height(200)
+			;
+
+
+	var e = svg.selectAll("rect").data(timings).enter();
+
+	e.append("rect") //SHOW BAR
+			.x(start)
+			.y(yLine)
+			.width(duration)
+			.height(HEIGHT)
+			.fill(function(d, i){
+				return colors[i % colors.length];
+			})
+	;
+
+	e.append("rect")  //SHOW TIME MARKERS
+			.x(function(d){
+				return start(d) - 0.5;
+			})
+			.y(function(d){
+				return yLine(d) - 5;
+			})
+			.width(1)
+			.height(HEIGHT + 10)
+			.fill("black")
+	;
+	e.append("svg")  //SHOW NAME OF EACH STEP (CLIP TO svg)
+			.x(function(d){
+				return start(d) + BORDER
+			})
+			.y(function(d){
+				return yLine(d) + BORDER
+			})
+			.width(function(d){
+				return Math.max(0, duration(d) - BORDER - BORDER)
+			})
+			.height(HEIGHT - BORDER - BORDER)
+			.append("text")
+			.text(function(d){
+				return coalesce(Map.get(d, "harness.step"), d.builder.step);
+			})
+			.y(HEIGHT - (BORDER * 3))
+			.fill('black')
+	;
+
+	e.append("g")  //SHOW START TIMES FOR EACH STEP
+			.translate(5, -15)
+			.rotate(45)
+			.translate(
+					function(d){
+						return start(d) + BORDER
+					},
+					function(d){
+						return yLine(d) + HEIGHT + BORDER
+					}
+			)
+			.append("text")
+			.text(function(d){
+				if (duration(d) < 20 || level(d) == 0) return "";
+				var s = coalesce(Map.get(d, "harness.start_time"), Map.get(d, "builder.start_time"));
+				return Duration.newInstance(1000 * (s - action.start_time)).format("HH:mm:ss");
+			})
+			.y(HEIGHT - (BORDER * 3))
+			.fill('black')
+	;
+
+	(function(d){
+		svg.append("g")  //SHOW end_time
+				.translate(5, -15)
+				.rotate(45)
+				.translate(end(d) + BORDER, LINE_PADDING + HEIGHT + LINE_PADDING + HEIGHT + BORDER)
+				.append("text")
+				.text(Duration.newInstance(1000 * (action.end_time - action.start_time)).format("HH:mm:ss"))
+				.y(HEIGHT - (BORDER * 3))
+				.fill('black')
+		;
+
+
+	})(timings.last());
+
+
+	e.append("rect")//INVISIBLE HOVER CATCHERS (IDENTICAL TO BAR SEGMENTS)
+			.x(start)
+			.y(yLine)
+			.width(duration)
+			.height(HEIGHT)
+			.fill("transparent")
+			.on("mouseover", function(d){
+				return timelineTip
+						.style({
+							"top": (d3.event.pageY + 10) + "px",
+							"left": (d3.event.pageX + 10) + "px",
+							"visibility": "visible"
+						})
+						.html(new Template('{{step}}<br>Duration = {{duration|format("H:mm:ss")}}').expand({
+							"step": coalesce(Map.get(d, "harness.step"), Map.get(d, "builder.step")),
+							"duration": Duration.newInstance(1000 * coalesce(Map.get(d, "harness.duration"), Map.get(d, "builder.duration")))
+						}))
+						;
+			})
+			.on("mousemove", function(d){
+				return timelineTip.style({
+					"top": (d3.event.pageY + 10) + "px",
+					"left": (d3.event.pageX + 10) + "px",
+				});
+			})
+			.on("mouseout", function(d){
+				return timelineTip.style("visibility", "hidden");
+			})
+	;
+
+}//showTimeline
+
+
+var timelineTip = d3.select("body").append("div")
+	.attr("id", "tip")
+	.style({
+		"padding": "5px 10px 5px 10px",
+		"position": "absolute",
+		"visibility": "hidden",
+		"background-color": "black",
+		"color": "white",
+		"font-weight": "bold",
+		"align-text": "center"
+	})
+	.html("")
+	;
+
 

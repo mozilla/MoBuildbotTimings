69d12a536c4a8b3d69ef07e6616a1160a7c4740b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-16 08:33:12 -0500

    whitespace on lib fix merge problems

--- modevlib/qb/Expressions.js
+++ modevlib/qb/Expressions.js
@@ -19,34 +19,81 @@ function qb2function(expr){
         return Map.get(value, expr);
       }
     }//endif
-  };
+  }//endif
 
-  if (expr.eq) {
-    if (isArray(expr.eq)) {
-      var exprs = expr.eq.map(qb2function);
-      return function(value){
-        return exprs[0](value) == exprs[1](value);
-      };
-    } else {
-      return function(value){
-        return Array.AND(Map.map(expr.eq, function(k, v){
-          return Map.get(value, k) == v;
-        }));
-      };
-    }//endif
-  } else if (expr.when) {
+  var keys = Object.keys(expr);
+  for (var i = keys.length; i--;) {
+    var val = expressions[keys[i]];
+    if (val) return val(expr);
+  }//for
+
+  Log.error("Can not handle expression `{{name}}`", {"name": keys[0]});
+}//method
+
+
+expressions = {};
+
+expressions.when = function(expr){
     var test = qb2function(expr.when);
     var pass = qb2function(expr.then);
     var fail = qb2function(expr.else);
     return function(value){
-      if (test(value)) {
+      var condition=test(value);
+
+      if (condition!=null && condition!=false) {
         return pass(value);
       } else {
         return fail(value);
       }//endif
     };
+};
+
+
+expressions.eq = function (expr) {
+  if (isArray(expr.eq)) {
+    var exprs = expr.eq.map(qb2function);
+    return function (value) {
+      return exprs[0](value) == exprs[1](value);
+    };
   } else {
-    var key = Map.getItems(expr).first();
-    Log.error("Can not handle expression `{{name}}`", {"name": key});
+    return function (value) {
+      return Array.AND(Map.map(expr.eq, function (k, v) {
+        return Map.get(value, k) == v;
+      }));
+    };
   }//endif
-}//method
+};
+
+
+expressions.literal=function(expr) {
+  return expr.literal;
+};
+
+expressions.case = function(expr){
+  var test = [];
+  var then = [];
+  var els_ = function(){};
+
+  expr.case.forall(function(s, i, switchs){
+    if (i==switchs.length-1){
+      els_ = qb2function(s);
+    }else{
+      test[i] = qb2function(s.when);
+      then[i] = qb2function(s.then);
+    }//endif
+  });
+
+  //(function(test, then, els_){
+    return function(value){
+      for (var i = 0; i < test.length; i++) {
+        var cond = test[i](value);
+        cond = cond != null && cond != false;
+        if (cond) {
+          return then[i](value);
+        }//endoif
+      }//for
+      return els_(value);
+    };
+  //})(test, then, els_);
+
+};
